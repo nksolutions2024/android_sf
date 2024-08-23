e03711686f3998c551590038c2694b66c3f4f532
acersf <sdsoni2042@gmail.com>
2024-08-23 19:09:26 +0530

    sapp in A50

---
+++ app/src/main/java/com/example/sapp1035/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.sapp1035;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Enable fullscreen mode
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_FULLSCREEN |
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+
+        );
+    }
+}
